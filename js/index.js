f8519500bc704556c2c522069e03e7640d07f60b
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-11-23 11:25:05 +0100

    Move book class to a separate file

--- js/index.js
+++ js/index.js
@@ -1,59 +1,54 @@
-// eslint-disable-next-line max-classes-per-file
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
+import Book from './book.js'
+
 class BookOperation {
-     static books = JSON.parse(localStorage.getItem('books') || '[]');
+  static books = JSON.parse(localStorage.getItem('books') || '[]');
 
-    static bookList = document.querySelector('#book-list');
+  static bookList = document.querySelector('#book-list');
 
-    static titleElement = document.querySelector('#title');
+  static titleElement = document.querySelector('#title');
 
-    static authorElement = document.querySelector('#author');
+  static authorElement = document.querySelector('#author');
 
-    static addButton = document.getElementById('add-button');
+  static addButton = document.getElementById('add-button');
 
-    static addBook(book) {
-      BookOperation.books.push(book);
-      localStorage.setItem('books', JSON.stringify(BookOperation.books));
-    }
+  static addBook(book) {
+    BookOperation.books.push(book);
+    localStorage.setItem('books', JSON.stringify(BookOperation.books));
+  }
 
-    static removeBook(book) {
-      BookOperation.books = BookOperation.books.filter((bookEl) => bookEl !== book);
-      localStorage.setItem('books', JSON.stringify(BookOperation.books));
-    }
+  static removeBook(book) {
+    BookOperation.books = BookOperation.books.filter((bookEl) => bookEl !== book);
+    localStorage.setItem('books', JSON.stringify(BookOperation.books));
+  }
 
-    static addBookElement(book, addBook) {
-      addBook(book);
-      const li = document.createElement('li');
-      const titleP = document.createElement('p');
-      titleP.innerText = book.title;
-      const authorP = document.createElement('p');
-      authorP.innerText = book.author;
-      const removeButton = document.createElement('button');
-      removeButton.addEventListener('click', () => {
-        BookOperation.removeBook(book);
-        li.remove();
-      });
+  static addBookElement(book, addBook) {
+    addBook(book);
+    const li = document.createElement('li');
+    const titleP = document.createElement('p');
+    titleP.innerText = book.title;
+    const authorP = document.createElement('p');
+    authorP.innerText = book.author;
+    const removeButton = document.createElement('button');
+    removeButton.addEventListener('click', () => {
+      BookOperation.removeBook(book);
+      li.remove();
+    });
 
-      removeButton.innerText = 'REMOVE';
-      li.appendChild(titleP);
-      li.appendChild(authorP);
-      li.appendChild(removeButton);
-      BookOperation.bookList.appendChild(li);
-    }
+    removeButton.innerText = 'REMOVE';
+    li.appendChild(titleP);
+    li.appendChild(authorP);
+    li.appendChild(removeButton);
+    BookOperation.bookList.appendChild(li);
+  }
 
-    static addNewBookElement() {
-      const title = BookOperation.titleElement.value;
-      BookOperation.titleElement.value = '';
-      const author = BookOperation.authorElement.value;
-      BookOperation.authorElement.value = '';
-      const newBook = new Book(title, author);
-      BookOperation.addBookElement(newBook, BookOperation.addBook);
-    }
+  static addNewBookElement() {
+    const title = BookOperation.titleElement.value;
+    BookOperation.titleElement.value = '';
+    const author = BookOperation.authorElement.value;
+    BookOperation.authorElement.value = '';
+    const newBook = new Book(title, author);
+    BookOperation.addBookElement(newBook, BookOperation.addBook);
+  }
 }
 
 BookOperation.addButton.addEventListener('click', BookOperation.addNewBookElement);
